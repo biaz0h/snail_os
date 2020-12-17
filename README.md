d6e3cd87bc8aa3204bfcba3927d938c14f58dd05
levin <eric.l.biazo@gmail.com>
2020-12-16 21:53:51 -0500

    added bare rust

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Snail OS
 
-Snail OS is my first OS in RUST
+Snail OS is my first OS in Rust
 
 # TODO:
 * Refactor Bootloader. Its crap. (Might switch to something like u-boot idk)
 * Get freestanding rust project
 * Get minimal kernel that prints stuff to screen
+
+# Referecences
+* https://github.com/tuhdo/os01/blob/master/Operating_Systems_From_0_to_1.pdf
+* https://github.com/cfenollosa/os-tutorial
+* https://os.phil-opp.com/
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+#![no_std] // don't link to std lib
+
+// Disable rust entry points
+// Also need ot disable c entry points via .cargo/config.toml
+#![no_main] 
+
 use core::panic::PanicInfo;
 
-#![no_std]
-fn main() {
+// Don't mangle _start
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
 
+// Create panic handler
 #[panic_handler]
-fn panic(_info: &panicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
